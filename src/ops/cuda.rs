2698dde23bf96efb4c4d1ca05335fe2244c02517
Peter Jin <peterhaijin@gmail.com>
2017-03-19 16:22:02 -0700

    Copy-paste the Apache 2.0 boilerpate in every source file.

--- src/ops/cuda.rs
+++ src/ops/cuda.rs
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 the arraydiff authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 use ffi::*;
 use ops::*;
 use prelude::*;
@@ -3179,21 +3195,23 @@ impl AutodiffOp for SoftmaxLoss<DeviceBatchArray1d<f32>, DeviceIoBatch<u32>, Dev
         Some(t) => t,
       };
       self.loss.val.get_excl(txn, node).set_batch_size(batch_sz);
-      let conn = DeviceStream::implicit().conn();
-      self.prob.val.get_excl(txn, node).as_view().wait(&conn);
-      target.val.get(txn, node).as_ref().wait(&conn);
-      self.loss.val.get_excl(txn, node).as_mut().wait(&conn);
-      unsafe { arraydiff_cuda_kernel_softmax_nll_loss_fwd_f32(
-          x_dim,
-          batch_sz,
-          self.prob.val.get_excl(txn, node).as_view().as_ptr(),
-          target.val.get(txn, node).as_ref().as_ptr(),
-          self.loss.val.get_excl(txn, node).as_mut().as_mut_ptr(),
-          conn.raw_stream().as_ptr(),
-      ) };
-      self.prob.val.get_excl(txn, node).as_view().post(&conn);
-      target.val.get(txn, node).as_ref().post(&conn);
-      self.loss.val.get_excl(txn, node).as_mut().post(&conn);
+      {
+        let conn = DeviceStream::implicit().conn();
+        self.prob.val.get_excl(txn, node).as_view().wait(&conn);
+        target.val.get(txn, node).as_ref().wait(&conn);
+        self.loss.val.get_excl(txn, node).as_mut().wait(&conn);
+        unsafe { arraydiff_cuda_kernel_softmax_nll_loss_fwd_f32(
+            x_dim,
+            batch_sz,
+            self.prob.val.get_excl(txn, node).as_view().as_ptr(),
+            target.val.get(txn, node).as_ref().as_ptr(),
+            self.loss.val.get_excl(txn, node).as_mut().as_mut_ptr(),
+            conn.raw_stream().as_ptr(),
+        ) };
+        self.prob.val.get_excl(txn, node).as_view().post(&conn);
+        target.val.get(txn, node).as_ref().post(&conn);
+        self.loss.val.get_excl(txn, node).as_mut().post(&conn);
+      }
     }
   }
 
